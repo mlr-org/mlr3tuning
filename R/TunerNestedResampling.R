679100cf7f87a6acc38721a7c73145ec8ecde6d5
schalkdaniel <d-schalk@t-online.de>
2018-12-21 16:25:34 +0100

    update NestedResampling

--- R/TunerNestedResampling.R
+++ R/TunerNestedResampling.R
@@ -1,71 +1,62 @@
-library(R6)
-library(mlr3)
-library(data.table)
-devtools::load_all()
-
 TunerNestedResampling = R6Class("TunerNestedResampling",
   inherit = Tuner,
   public = list(
-    tuner = NULL,
-    resampling_outer = NULL,
+    inner_tuner = NULL,
+    outer_resampling = NULL,
     bmr_outer = NULL,
-    # param_best = NULL,
-    # terminator = NULL, # in parent class
 
-    initialize = function(tuner, resampling) {
-      self$tuner = checkmate::assert_r6(tuner, "Tuner")
-      self$resampling_outer = checkmate::assert_r6(resampling, "Resampling")
-      self$resampling_outer$instantiate(tuner$ff$task)
-      self$terminator = TerminatorIterations$new(resampling$iters)
-      self$ff = tuner$ff$clone()
+    initialize = function(inner_tuner, outer_resampling) {
+      self$inner_tuner = checkmate::assert_r6(inner_tuner, "Tuner")
+
+      self$outer_resampling = checkmate::assert_r6(outer_resampling, "Resampling")
+      self$outer_resampling$instantiate(inner_tuner$ff$task)
+      
+      self$terminator = TerminatorIterations$new(outer_resampling$iters)
+      
+      self$ff = inner_tuner$ff$clone()
       self$ff$resampling$instantiate(self$ff$task)
-      # self$ff$resampling = assert_r6(resampling, "Resampling")
+
+      # Necessary to get correct reference of the terminator for the outer resampling.
+      self$ff$hooks$update_start = list(self$terminator$update_start)
+      self$ff$hooks$update_end = list(self$terminator$update_end)
     }
   ),
 
   private = list(
     tune_step = function() {
-      tuner_temp = self$tuner$clone()
+      browser()
+      
+      # Deep copy is necessary to correctly copy the terminator of the inner tuningQ
+      tuner_temp = self$inner_tuner$clone(deep = TRUE)
+
+      # Necessary to get the correct reference from the tuner_temp terminator in the hooks. Otherwise,
+      # the hook terminator reference points to the terminator of self$inner_tuner. This results in that the
+      # tuner_temp uses his own terminator object to stop the tuning, but the hook reduces the number of remaining 
+      # iterations of the self$inner_tuning terminator. Therefore, tuning is not stopped.
+      tuner_temp$ff$hooks$update_start = list(tuner_temp$terminator$update_start)
+      tuner_temp$ff$hooks$update_end = list(tuner_temp$terminator$update_end)
+
       # +1 because the terminator starts counting with 0
-      train_set_temp = self$ff$resampling$train_set(self$terminator$state$evals + 1L)
+      iter = self$terminator$state$iters + 1L
+      train_set_temp = self$outer_resampling$train_set(iter)
       tuner_temp$ff$task$filter(train_set_temp)
-      tuner_remp$tune()
+      tuner_temp$tune()
 
-      # Fill param_best with best parameter setting from previous tuning
-
-      resampling_temp = ResamplignCustom$new()
-      resampling_temp$instantiate(self$ff$task, train_set_temp)
+      # FIXME: ResamplingCustom does not work at the moment see https://github.com/mlr-org/mlr3/issues/108
+      # resampling_temp = ResamplingCustom$new()
+      # resampling_temp$instantiate(self$ff$task, train_sets = list(train_set_temp))
+      resampling_temp = ResamplingHoldout$new()
       self$ff$resampling = resampling_temp
       self$ff$eval(tuner_temp$tune_result()$param_vals)
+
+      # FIXME: include tuner_temp to the data of the benchmark result of the Fitness Function of self:
+
+      # if (nrow(self$ff$bmr$data) == 1) {
+      #   self$ff$bmr$data$inner_tuner = tuner_temp
+      # } else {
+      #   self$ff$bmr$data$inner_tuner[iter] = tuner_temp
+      # }
     }
   )
 )
 
-# task = mlr3::mlr_tasks$get("spam")
-# learner = mlr3::mlr_learners$get("classif.rpart")
-# learner$predict_type = "prob"
-# resampling = mlr3::mlr_resamplings$get("holdout")
-# measures = mlr3::mlr_measures$mget(c("auc", "mmce"))
-# param_set = paradox::ParamSet$new(
-#   params = list(
-#     paradox::ParamDbl$new("cp", lower = 0.001, upper = 0.1),
-#     paradox::ParamInt$new("minsplit", lower = 2, upper = 5)
-#   ),
-# )
-
-# ff = FitnessFunction$new(
-#   task = task,
-#   learner = learner,
-#   resampling = resampling,
-#   measures = measures,
-#   param_set = param_set,
-#   ctrl = tune_control(store_prediction = TRUE) # for the exceptions
-# )
-
-# terminator = TerminatorEvaluations$new(5)
-# gs = TunerRandomSearch$new(ff, terminator)
-# # gs$tune()
-
-# outer = mlr_resamplings$get("cv")
-# nested = TunerNestedResampling$new(gs, outer)
-# nested$tune()
