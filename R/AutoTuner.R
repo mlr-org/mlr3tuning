ed7dad7296023a3d9afeabec66615684251181d0
schalkdaniel <d-schalk@t-online.de>
2019-01-04 15:58:53 +0100

    initial auto tuner stuff

---
+++ R/AutoTuner.R
@@ -0,0 +1,129 @@
+#' @title AutoTuner
+#'
+#' @description
+#' Tuner child class to conduct grid search.
+#'
+#' @section Usage:
+#' ```
+#' # Construction
+#' at = AutoTuner$new(learner, resampling, param_set, terminator, 
+#'   tuner, tuner_settings, ctrl = tune_control(), id = "autotuner")
+#' 
+#' # public fields
+#' at$learner
+#' ```
+#' See [Learner] for a description of the interface.
+#'
+#' @section Arguments:
+#' * `learner` ([Learner]): \cr
+#'   Internal learner that is tuned and finally returned as trained learner with optimal parameter configuration.
+#' * `resampling` ([Resampling]): \cr
+#'   Resampling strategy for the tuning.
+#' * `param_set` ([paradox::ParamSet]) \cr
+#'   Parameter set for the tuning.
+#' * `terminator` ([Terminator]) \cr 
+#'   Terminator used to stop the tuning.
+#' * `tuner` (Tuner Class Generator) \cr
+#'   Uninitialized tuner factory, e.g. TunerGridSearch.
+#' * `tuner_settings` (named list) \cr
+#'   List with tuner settings (e.g. see `?TunerGridSearch`)
+#'
+#' @section Details:
+#'
+#' The interface is described in [Learner].
+#' 
+#' @name AutoTuner
+#' @keywords internal
+#' @family Learner
+#' @examples
+#' task = mlr3::mlr_tasks$get("iris")
+#' learner = mlr3::mlr_learners$get("classif.rpart")
+#' resampling = mlr3::mlr_resamplings$get("holdout")
+#' measures = mlr3::mlr_measures$mget("mmce")
+#' task$measures = measures
+#' param_set = paradox::ParamSet$new(
+#'   params = list(paradox::ParamDbl$new("cp", lower = 0.001, upper = 0.1)))
+#' 
+#' terminator = TerminatorEvaluations$new(5)
+#' 
+#' at = AutoTuner$new(learner, resampling, param_set, terminator, tuner = TunerGridSearch, 
+#'   tuner_settings = list(resolution = 10L))
+#' 
+#' at$train(task)
+#' at$learner
+NULL
+
+#' @export
+#' @include Learner.R
+AutoTuner = R6Class("AutoTuner", inherit = Learner,
+  public = list(
+    learner = NULL,
+
+    initialize = function (learner, resampling, param_set, terminator, tuner, tuner_settings, ctrl = tune_control(), id = "autotuner") {
+      self$learner = mlr3::assert_learner(learner = learner)
+
+      # Shit!!!!
+      private$.terminator = checkmate::assert_r6(terminator, "Terminator")
+      private$.tuner_settings = tuner_settings
+      private$.ff_args$resampling = mlr3::assert_resampling(resampling)
+      private$.ff_args$param_set = checkmate::assert_class(param_set, "ParamSet")
+      private$.ff_args$ctrl = checkmate::assert_list(ctrl, names = "unique")
+
+      # TODO: Check for factory
+      if (! inherits(tuner, "R6ClassGenerator") && grepl(pattern = "Tuner", x = tuner$classname))
+        mlr3misc::stopf("Tuner must be a R6 class generator that creates tuner (e.g. TunerGridSearch).")
+      private$.tuner = tuner
+
+      super$initialize(
+        id = id,
+        task_type = self$learner$task_type,
+        packages = self$learner$packages,
+        feature_types = self$learner$feature_types,
+        predict_types = self$learner$predict_types,
+        param_set = self$learner$param_set,
+        properties = self$learner$properties
+      )
+    },
+
+    train = function (task) {
+      task = mlr3::assert_task(task)
+      self$learner = mlr3::assert_learner(learner = learner, task = task)
+
+      private$.tuner_settings$terminator = private$.terminator$clone()
+      private$.tuner_settings$ff = FitnessFunction$new(task = task, learner = self$learner, resampling = private$.ff_args$resampling,
+        param_set = private$.ff_args$param_set, ctrl = private$.ff_args$ctrl)
+
+      private$.tuner = do.call(private$.tuner$new, private$.tuner_settings)
+      private$.tuner$tune()
+
+      self$learner$param_vals = private$.tuner$tune_result()$param_vals
+      self$learner$train(task)
+      
+      private$.is_instantiated = TRUE
+
+      return (invisible(self))
+    },
+
+    predict = function (task) {
+      return (self$learner$predict(task))
+    }
+  ),
+
+  private = list(
+    .ff_args = NULL,
+    .terminator = NULL,
+    .tuner = NULL,
+    .tuner_settings = NULL,
+    .is_instantiated = FALSE
+  ),
+
+  active = list(
+    tuner = function () {
+      if (private$.is_instantiated) {
+        return (private$.tuner)
+      } else {
+        return (NULL)
+      }
+    }
+  )
+)
---
+++ tests/testthat/test_AutoTuner.R
@@ -0,0 +1,58 @@
+context("TunerNestedResampling")
+
+test_that("TunerNestedResampling",  {
+  outer_folds = 3L
+  inner_folds = 4L
+  inner_evals = 5L
+
+  p_measures = c("mmce", "time_train", "time_both")
+
+  task = mlr3::mlr_tasks$get("iris")
+
+  learner = mlr3::mlr_learners$get("classif.rpart")
+
+  resampling = mlr3::mlr_resamplings$get("cv")
+  resampling$param_vals = list(folds = inner_folds)
+
+  measures = mlr3::mlr_measures$mget(p_measures)
+  task$measures = measures
+
+  param_set = paradox::ParamSet$new(params = list(
+    paradox::ParamDbl$new("cp", lower = 0.001, upper = 0.1
+      )))
+
+  terminator = TerminatorEvaluations$new(inner_evals)
+
+  at = AutoTuner$new(learner, resampling, param_set, terminator, tuner = TunerGridSearch, 
+    tuner_settings = list(resolution = 10L))
+
+  # Nested Resampling:
+  outer_resampling = mlr3::mlr_resamplings$get("cv")
+  outer_resampling$param_vals = list(folds = outer_folds)
+  r = resample(task, at, outer_resampling)
+
+  at$tuner
+  at$train(task)
+  at$tuner
+  at$predict(task)
+
+  expect_equal(at$learner$param_vals, at$tuner$tune_result()$param_vals)
+
+  # Nested Resampling:
+  checkmate::expect_data_table(r$data, nrow = outer_folds)
+  lapply(r$data$learner, function (autotuner) {
+    checkmate::expect_data_table(autotuner$tuner$ff$bmr$data, nrow = inner_evals * inner_folds)
+    checkmate::expect_data_table(autotuner$tuner$ff$bmr$aggregated, nrow = inner_evals)
+    expect_equal(names(autotuner$tune_result()$performance), p_measures)
+  })
+
+  row_ids_inner = lapply(r$data$learner, function (it) {
+    it$tuner$ff$task$row_ids[[1]]
+  })
+  row_ids_all = task$row_ids[[1]]
+  
+  expect_equal(sort(unique(unlist(row_ids_inner))), sort(row_ids_all))
+  nuisance = lapply(row_ids_inner, function (ids) {
+    expect_true(any(! row_ids_all %in% ids))
+  })
+})
