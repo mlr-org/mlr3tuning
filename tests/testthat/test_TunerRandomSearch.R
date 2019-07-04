b9ae6751ece080b19934fc95a19d4bf22137bc9f
Michel Lang <michellang@gmail.com>
2019-07-05 00:17:48 +0200

    added test and fix #40

--- tests/testthat/test_TunerRandomSearch.R
+++ tests/testthat/test_TunerRandomSearch.R
@@ -31,3 +31,22 @@ test_that("TunerRandomSearch", {
   expect_list(result$values, len = 2)
   expect_equal(result$values$minsplit, 3)
 })
+
+test_that("aggregate one row (#40)", {
+  pe = PerformanceEvaluator$new(
+    task = mlr3::mlr_tasks$get("iris"),
+    learner = mlr3::mlr_learners$get("classif.rpart"),
+    resampling = mlr3::mlr_resamplings$get("cv"),
+    measures = mlr3::mlr_measures$get("classif.ce"),
+    param_set = ParamSet$new(params = list(
+      ParamDbl$new("cp", lower = 0.01, upper = 0.1)
+    ))
+  )
+
+  terminator_1 = TerminatorEvaluations$new(max_evaluations = 1)
+  tuner_1 = TunerRandomSearch$new(pe, terminator = terminator_1, batch_size = 1)
+  tuner_1$tune()
+  tab = tuner_1$aggregate()
+  expect_data_table(tab, nrow = 1)
+  expect_number(tab$classif.ce)
+})
