0338cca450e36baec973c3697eea002b46aafff0
Michel Lang <michellang@gmail.com>
2019-08-07 22:50:02 +0200

    fix for oldrel

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -4,6 +4,10 @@ old_opts = options(
   warnPartialMatchAttr = TRUE,
   warnPartialMatchDollar = TRUE
 )
+
+# https://github.com/HenrikBengtsson/Wishlist-for-R/issues/88
+old_opts = lapply(old_opts, function(x) if (is.null(x)) FALSE else x)
+
 lg = lgr::get_logger("mlr3")
 old_threshold = lg$threshold
 lg$set_threshold("warn")
