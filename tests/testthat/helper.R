57eb09494d352217c8dd485ff783479d718aad08
Bernd Bischl <bernd_bischl@gmx.net>
2019-08-08 02:38:37 +0200

    GridSearch: finer unit tests

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -8,12 +8,24 @@ expect_terminator = function(term) {
 
 # test an implemented subclass tuner by running a couple of standard tests
 # on a simple example
-test_tuner = function(tuner_factory, arg_list = list(), n_evals = 5L) {
-  ps = ParamSet$new(params = list(
-    ParamDbl$new("cp", lower = 0.001, upper = 0.1)
-  ))
+# term_evals: how we configure the Terminator
+# real_evals: how many evals we really expect (as the optim might early stop)
+# returns: tuner, so we can investgate its state more in individual tests
+test_tuner = function(tuner_factory, arg_list = list(), n_dim = 1L,
+  term_evals = 5L, real_evals = term_evals) {
+
+  ps = if (n_dim == 1) {
+    ParamSet$new(params = list(
+      ParamDbl$new("cp", lower = 0.1, upper = 0.3)
+    ))
+  } else if (n_dim == 2) {
+    ParamSet$new(params = list(
+      ParamDbl$new("cp", lower = 0.1, upper = 0.3),
+      ParamInt$new("minsplit", lower = 1, upper = 9)
+    ))
+  }
   pe = PerfEval$new("iris", "classif.rpart", "holdout", "classif.ce", ps)
-  term = TerminatorEvaluations$new(n_evals)
+  term = TerminatorEvaluations$new(term_evals)
   arg_list_2 = list(pe = pe, terminator = term)
   arg_list_2 = insert_named(arg_list_2, arg_list)
   tuner = do.call(tuner_factory$new, arg_list_2)
@@ -22,11 +34,12 @@ test_tuner = function(tuner_factory, arg_list = list(), n_evals = 5L) {
   r = tuner$tune_result()
   bmr = tuner$pe$bmr
 
-  expect_data_table(bmr$data, nrows = n_evals)
-  expect_equal(pe$n_evals, n_evals)
+  expect_data_table(bmr$data, nrows = real_evals)
+  expect_equal(pe$n_evals, real_evals)
   expect_list(r)
   expect_number(r$performance["classif.ce"], lower = 0, upper = 1)
-  expect_list(r$values, len = 2L)
+  expect_list(r$values, len = n_dim + 1)
+  return(tuner)
 }
 
 
--- tests/testthat/test_TunerGridSearch.R
+++ tests/testthat/test_TunerGridSearch.R
@@ -2,7 +2,13 @@ context("TunerGridSearch")
 
 
 test_that("TunerGridSearch", {
-  reso = 7L
-  test_tuner(TunerGridSearch, list(resolution = reso))
+  tuner = test_tuner(TunerGridSearch, list(resolution = 7), term_evals = 5L, real_evals = 5, n_dim = 1)
+
+  tuner = test_tuner(TunerGridSearch, list(resolution = 3), term_evals = 999L, real_evals = 9, n_dim = 2)
+  expect_equal(tuner$pe$n_evals, 9)
+  a = tuner$aggregate()
+  expect_data_table(a, nrows = 9)
+  expect_set_equal(unique(a$cp), c(0.1, 0.2, 0.3))
+  expect_set_equal(unique(a$minsplit), c(1, 5, 9))
 })
 
