9f9ef40d4ea3286f852bcf5fe8dd094825aee377
Bernd Bischl <bernd_bischl@gmx.net>
2019-08-09 03:18:39 +0200

    GS: add param_resolutions arg

--- R/TunerGridSearch.R
+++ R/TunerGridSearch.R
@@ -25,6 +25,8 @@
 #' * `resolution` :: `integer(1)`\cr
 #'   Resolution of the grid, see [paradox::generate_design_grid].
 #'   Stored in `settings`.
+#' * `param_resolutions` :: named `integer()` \cr
+#'   Resolution per param, named by parameter ID, see [paradox::generate_design_grid].
 #' * `batch_size` :: `integer(1)`\cr
 #'   Maximum number of configurations to try in a batch.
 #'   Stored in `settings`.
@@ -37,10 +39,35 @@
 TunerGridSearch = R6Class("TunerGridSearch",
   inherit = Tuner,
   public = list(
-    initialize = function(pe, terminator = NULL, resolution = 10L, batch_size = 1L) {
-      resolution = assert_int(resolution, lower = 1L, coerce = TRUE)
+    initialize = function(pe, terminator = NULL, resolution = 10L, param_resolutions = NULL, batch_size = 1L) {
+
+      # argcheck resolution and param_reolution, code is copy-paste from paradox, but better check the validity in constructor
+      ids = pe$param_set$ids()
+      ids_num = ids[pe$param_set$is_number]
+      par_res = integer(0L) # here we construct the resolution for each param
+      if (length(ids_num) > 0L) { # if only categ we dont need to check
+        if (is.null(resolution) && is.null(param_resolutions)) {
+          stop("You must specify 'resolution' or 'param_resolutions'!")
+        }
+        if (!is.null(resolution)) {
+          # create param_resolutions list, constant entry, same length as ids and named with ids
+          resolution = assert_count(resolution, positive = TRUE, coerce = TRUE)
+          par_res = set_names(rep.int(resolution, pe$param_set$length), ids)
+        }
+        if (!is.null(param_resolutions)) {
+          assert_integerish(param_resolutions, lower = 1L, any.missing = FALSE, coerce = TRUE)
+          assert_names(names(param_resolutions), subset.of = ids_num) # user only needs to pass num params (categ resolutions are overwritten anyway)
+          par_res = insert_named(par_res, param_resolutions)
+        }
+        ids_miss = setdiff(ids_num, names(par_res))
+        if (length(ids_miss) > 0L) {
+          stopf("Resolution settings missing for some numerical params: %s", str_collapse(ids_miss))
+        }
+      }
+      # argcheck end for resolution
+
       batch_size = assert_int(batch_size, lower = 1L, coerce = TRUE)
-      s = list(batch_size = batch_size, resolution = resolution)
+      s = list(batch_size = batch_size, resolution = resolution, param_resolutions = param_resolutions)
       super$initialize(pe = pe, terminator = terminator, settings = s)
       return(self)
     }
@@ -48,7 +75,7 @@ TunerGridSearch = R6Class("TunerGridSearch",
 
   private = list(
     tune_internal = function() {
-      g = generate_design_grid(self$pe$param_set, resolution = self$settings$resolution)
+      g = generate_design_grid(self$pe$param_set, resolution = self$settings$resolution, param_resolutions = self$settings$param_resolutions)
       ch = chunk_vector(1:nrow(g$data), chunk_size = self$settings$batch_size, shuffle = TRUE)
       for (i in 1:length(ch)) {
         self$eval_batch(g$data[ch[[i]],])
--- tests/testthat/test_TunerGridSearch.R
+++ tests/testthat/test_TunerGridSearch.R
@@ -2,13 +2,20 @@ context("TunerGridSearch")
 
 
 test_that("TunerGridSearch", {
-  tuner = test_tuner(TunerGridSearch, list(resolution = 7), term_evals = 5L, real_evals = 5, n_dim = 1)
+  tuner = test_tuner(TunerGridSearch, list(resolution = 7), term_evals = 5L, real_evals = 5, n_dim = 1L)
 
-  tuner = test_tuner(TunerGridSearch, list(resolution = 3), term_evals = 999L, real_evals = 9, n_dim = 2)
+  tuner = test_tuner(TunerGridSearch, list(resolution = 3), term_evals = 999L, real_evals = 9, n_dim = 2L)
   expect_equal(tuner$pe$n_evals, 9)
   a = tuner$archive()
   expect_data_table(a, nrows = 9)
   expect_set_equal(unique(a$cp), c(0.1, 0.2, 0.3))
+  expect_set_equal(unique(a$minsplit), c(1, 5, 9))
+
+  tuner = test_tuner(TunerGridSearch, list(param_resolutions = c(cp = 2L, minsplit = 3L)), term_evals = 999L, real_evals = 6L, n_dim = 2L)
+  expect_equal(tuner$pe$n_evals, 6L)
+  a = tuner$archive()
+  expect_data_table(a, nrows = 6L)
+  expect_set_equal(unique(a$cp), c(0.1, 0.3))
   expect_set_equal(unique(a$minsplit), c(1, 5, 9))
 })
 
