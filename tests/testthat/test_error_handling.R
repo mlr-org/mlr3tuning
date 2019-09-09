46128e20e521e8db0bc8ce571fc418d840f51133
Bernd Bischl <bernd_bischl@gmx.net>
2019-09-09 19:34:52 +0200

    merge fix

--- tests/testthat/test_error_handling.R
+++ tests/testthat/test_error_handling.R
@@ -35,8 +35,7 @@ test_that("predictions missing", {
 
   instance = TuningInstance$new(task = tsk("iris"), learner = learner, resampling = rsmp("holdout"),
     measures = msr("classif.ce"), param_set = param_set, terminator = term("evals", n_evals = 10))
-  tt$tune(instance)
-  expect_error(tt$tune_result(instance), "[Nn]o non-missing")
+  expect_error(tt$tune(instance), "[Nn]o non-missing")
 })
 
 
