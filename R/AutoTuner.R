19499744669f64a12f4b006d5fe85ac1839d92aa
Bernd Bischl <bernd_bischl@gmx.net>
2019-09-10 04:19:05 +0200

    mini iter AutoTuner

--- R/AutoTuner.R
+++ R/AutoTuner.R
@@ -4,50 +4,42 @@
 #' @format [R6::R6Class] object inheriting from [mlr3::Learner].
 #'
 #' @description
-#' The `AutoTuner` is a [mlr3::Learner] which tunes a subordinate learner via resampling.
-#' The best found configuration is then used to train a model on the complete training data.
+#' The `AutoTuner` is a [mlr3::Learner] which auto-tunes by first going tuning its hyperparameters
+#' on the training data, then setting the optimal configuration in the learner, then finally
+#' fitting the model on the complete training data.
 #' Note that this class allows to perform nested resampling by passing an [AutoTuner] object to [mlr3::resample()]
 #' or [mlr3::benchmark()].
 #'
 #' @section Construction:
 #' ```
-#' at = AutoTuner$new(learner, resampling, measures, param_set, terminator,
-#'   tuner, bm_args = list(), id = "autotuner")
+#' at = AutoTuner$new(learner, resampling, measures, tune_ps, terminator, tuner, bm_args = list())
 #' ```
 #' * `learner` :: [mlr3::Learner] | [mlr3::mlr_sugar]\cr
-#'   Learner to tune.
+#'   Learner to tune, see [TuningInstance].
 #' * `resampling` :: [mlr3::Resampling] | [mlr3::mlr_sugar]\cr
-#'   Resampling strategy used to assess the performance of the learner on the (subset of) the
+#'   Resampling strategy during tuning, see [TuningInstance].
 #' * `measures` :: list of [mlr3::Measure] | [mlr3::mlr_sugar]\cr
-#'   Performance measures. The first one is optimized.
-#' * `param_set` :: [paradox::ParamSet]\cr
-#'   Hyperparameter search space.
+#'   Performance measures. The first one is optimized, see [TuningInstance].
+#' * `tune_ps` :: [paradox::ParamSet]\cr
+#'   Hyperparameter search space, see [TuningInstance].
 #' * `terminator` :: [Terminator]\cr
-#'   When to stop tuning.
+#'   When to stop tuning, see [TuningInstance].
 #' * `tuner` :: [Tuner]\cr
 #'   Tuning algorithm to run.
 #' * `bm_args` :: `list()`\cr
 #'   Further arguments for [mlr3::benchmark()], see [TuningInstance].
-#' * `id` :: `character(1)`\cr
-#'   Name of the learner.
 #'
 #' @section Fields:
 #' All fields from [Learner], and additionally:
 #'
 #' * `learner` :: [mlr3::Learner]\cr
-#'   Subordinate learner. After `train()` of the `AutoTuner` has been executed,
-#'   this learner stores the final model and is parametrized with the best found solution.
 #' * `resampling` :: [mlr3::Resampling].
 #' * `measures` :: list of [mlr3::Measure].
 #' * `tune_ps` :: [paradox::ParamSet]\cr
-#'   Hyperparameter search space.
 #' * `terminator` :: [Terminator].
 #' * `tuner` :: [Tuner].
 #' * `store_bmr` :: `logical(1)`\cr
 #'   If `TRUE`, stores the benchmark result as slot `$bmr`.
-#' * `tune_path` :: [data.table::data.table()]\cr
-#'   Only stored if `store_bmr` has been set to `TRUE`.
-#'   This is the archive of the stored [mlr3::BenchmarkResult] with hyperparameters as separate columns.
 #'
 #' @section Methods:
 #' See [mlr3::Learner].
@@ -74,7 +66,6 @@
 #' at$learner
 AutoTuner = R6Class("AutoTuner", inherit = Learner,
   public = list(
-    # FIXME: doesnt thus store the instance? and hence the complete bmr and all other slots?
     learner = NULL,
     terminator = NULL,
     resampling = NULL,
@@ -83,29 +74,25 @@ AutoTuner = R6Class("AutoTuner", inherit = Learner,
     tune_ps = NULL,
     store_bmr = FALSE,
     bm_args = NULL,
-    # FIXME: state = bmr + learner
-    # FIXME: look at pipeopelearner for paramset
-    # FIXME: autotuner paramset darf eigentlich nicht die params mehr enthalten über die getuned wird
 
-    initialize = function(learner, resampling, measures, tune_ps, terminator, tuner, bm_args = list(), id = "autotuner") {
-      self$id = assert_string(id) # needs to be set first for param_set active binding
-      self$tuner = assert_r6(tuner, "Tuner")
-      self$learner = learner = assert_learner(learner = as_learner(learner, clone = TRUE))
-      self$learner$param_set$set_id = ""
-      self$terminator = assert_r6(terminator, "Terminator")
-      self$resampling = assert_resampling(as_resampling(resampling, clone = TRUE))
-      self$measures = assert_measures(as_measures(measures, clone = TRUE), learner = learner)
-      self$tune_ps = assert_class(tune_ps, "ParamSet")
+    initialize = function(learner, resampling, measures, tune_ps, terminator, tuner, bm_args = list()) {
+      self$learner = assert_learner(learner)
+      self$resampling = assert_resampling(resampling)
+      self$measures = assert_measures(as_measures(measures), learner = learner)
+      self$tune_ps = assert_param_set(tune_ps)
+      self$learner$param_set$set_id = "" # FIXME: i have no idea why we do this here?
+      self$terminator = assert_terminator(terminator)
+      self$tuner = assert_tuner(tuner)
       self$bm_args = assert_list(bm_args, names = "unique")
 
       super$initialize(
-        id = id,
-        task_type = learner$task_type,
-        packages = learner$packages,
-        feature_types = learner$feature_types,
-        predict_types = learner$predict_types,
-        param_set = learner$param_set,
-        properties = learner$properties
+        id = paste0(self$learner$id, ".tuned"),
+        task_type = self$learner$task_type,
+        packages = self$learner$packages,
+        feature_types = self$learner$feature_types,
+        predict_types = self$learner$predict_types,
+        param_set = self$learner$param_set,
+        properties = self$learner$properties
       )
     },
 
