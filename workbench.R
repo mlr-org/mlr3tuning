f69ed6c2c126f2144d834ed118560b0c8cf6183e
SebGruber1996 <gruber_sebastian@t-online.de>
2019-09-15 17:31:31 +0200

    remove pareto_front from workbench

--- workbench.R
+++ workbench.R
@@ -24,32 +24,3 @@ tuner$tune(inst)
 
 print(inst$archive())
 
-
-
-    pareto_front = function(measures = NULL) {
-
-      measure_ids = map_chr(measures, "id")
-
-      if (is.null(measures)) {
-        measures = self$measures
-      } else {
-        measures = lapply(measures, as_measure, task_type = self$task$task_type)
-        # check that we are only using contained measures
-        assert_choice(measure_ids, map_chr(self$measures, "id"))
-     }
-      # check that at leaste two measures are given
-      assert_true(length(measure_ids) > 1)
-      #assert_measure(measure, task = self$task, learner = self$learner)
-      #if (is.na(measure$minimize))
-      #  stopf("Measure '%s' has minimize = NA and hence cannot be tuned", measure$id)
-
-      tab = self$bmr$aggregate(measures, ids = FALSE)
-      y = tab[measure_ids]
-      #if (allMissing(y))
-      #  stopf("No non-missing performance value stored")
-
-      minimize = map_chr(measures, "minimize")
-      is_pareto = calculate_pareto_front(y, !minimize, return_data = FALSE)
-
-      result = tab$resample_result[is_pareto]
-    }
