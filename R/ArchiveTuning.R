2b7db33540916e3b61cbeca01e5dac51220257c7
Marc Becker <marcbecker@posteo.de>
2020-08-14 10:57:57 +0200

    Docu benchmark result

--- R/ArchiveTuning.R
+++ R/ArchiveTuning.R
@@ -4,8 +4,10 @@
 #' Container around a [data.table::data.table] which stores all performed
 #' function calls of the Objective and the associated [mlr3::BenchmarkResult].
 #'
-#' The [mlr3::BenchmarkResult] is connected to the [data.table::data.table] via
-#' the `uhashes` column.
+#' `$benchmark_result` stores a [mlr3::BenchmarkResult] which contains the
+#' [mlr3::ResampleResult] of all performaned function calls. The
+#' [mlr3::BenchmarkResult] is connected to the [data.table::data.table] via the
+#' `uhash` column.
 #'
 #' @section Technical details:
 #'
