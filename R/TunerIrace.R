1fc81b8e0c1f029c2b7446e05c3729ecad839634
Marc Becker <marcbecker@posteo.de>
2021-01-15 09:20:53 +0100

    Add error message if no result is found

--- R/TunerIrace.R
+++ R/TunerIrace.R
@@ -129,6 +129,9 @@ TunerIrace = R6Class("TunerIrace",
     # We store the best performing one.
     # The reported performance value is the average of all resampling iterations.
     .assign_result = function(inst) {
+      if(length(private$.result_id) == 0) {
+        stop("irace::irace did not return a result. The evaluated configurations are still accessible through the archive.")
+      }
       id_configuration = NULL
       res = inst$archive$data[id_configuration == private$.result_id, ]
       cols = c(inst$archive$cols_x, "id_configuration")
