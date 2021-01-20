ec9fd8f31a9805bb5df3325b2374aa44c90cae84
Marc Becker <marcbecker@posteo.de>
2021-01-20 18:38:18 +0100

    Load packages

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -1,2 +1,7 @@
+library(mlr3)
+library(R6)
+library(mlr3misc)
+library(mlr3pipelines)
+
 lapply(list.files(system.file("testthat", package = "mlr3"), pattern = "^helper.*\\.[rR]", full.names = TRUE), source)
 lapply(list.files(system.file("testthat", package = "mlr3tuning"), pattern = "^helper.*\\.[rR]", full.names = TRUE), source)
