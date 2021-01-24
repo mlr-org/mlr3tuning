25ed82efd862f2b9878a8108417a3fccea672e78
Marc Becker <33069354+be-marc@users.noreply.github.com>
2021-01-24 16:11:42 +0100

    nolint (#292)

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -1,3 +1,4 @@
+# nolint start
 library(mlr3)
 library(mlr3misc)
 library(mlr3pipelines)
@@ -6,3 +7,4 @@ library(R6)
 
 lapply(list.files(system.file("testthat", package = "mlr3"), pattern = "^helper.*\\.[rR]", full.names = TRUE), source)
 lapply(list.files(system.file("testthat", package = "mlr3tuning"), pattern = "^helper.*\\.[rR]", full.names = TRUE), source)
+# nolint end
