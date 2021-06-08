80405934bea5a342e57dd90b3eff82939ee7f418
Marc Becker <marcbecker@posteo.de>
2021-06-08 13:25:08 +0200

    restructure files

---
+++ R/tune.R
@@ -0,0 +1,62 @@
+#' @title Function for Tuning
+#' 
+#' @description
+#' Function to tune a [mlr3::Learner].
+#' 
+#' @param method (`character(1)`)\cr
+#'  Key to retrieve tuner from [mlr_tuners] dictionary.
+#' @param term_evals (`integer(1)`)\cr
+#'  Number of allowed evaluations.
+#' @param term_time (`integer(1)`)\cr
+#'  Maximum allowed time in seconds.
+#' @param ... (named `list()`)\cr
+#'  Named arguments to be set as parameters of the tuner.
+#' 
+#' @return `TuningInstanceSingleCrit`
+#'  
+#' @template param_task
+#' @template param_learner
+#' @template param_resampling
+#' @template param_measure
+#' @template param_search_space
+#' 
+#' @export 
+#' @examples
+#' learner = lrn("classif.rpart", cp = to_tune(1e-04, 1e-1, logscale = TRUE))
+#' 
+#' instance = tune(
+#'   method = "random_search", 
+#'   task = tsk("pima"), 
+#'   learner = learner, 
+#'   resampling = rsmp ("holdout"), 
+#'   measure = msr("classif.ce"), 
+#'   term_evals = 4) 
+#' 
+#' # apply hyperparameter values to learner
+#' learner$param_set$values = instance$result_learner_param_vals
+tune = function(method, task, learner, resampling, measure, term_evals = NULL, term_time = NULL, search_space = NULL,
+  ...) {
+  assert_choice(method, mlr_tuners$keys())
+  tuner = tnr(method, ...)
+  terminator = terminator_selection(term_evals, term_time)
+
+  instance = TuningInstanceSingleCrit$new(task, learner, resampling, measure, terminator, search_space)
+
+  tuner$optimize(instance)
+  instance
+}
+
+terminator_selection = function(term_evals, term_time) {
+  assert_int(term_evals, null.ok = TRUE)
+  assert_int(term_time, null.ok = TRUE)
+
+  if (is.null(term_evals) && is.null(term_time)) {
+    trm("none")
+  } else if (!is.null(term_evals) && !is.null(term_time)) {
+    trm("combo", list(trm("evals", n_evals = term_evals), trm("run_time", secs = term_time)))
+  } else if (!is.null(term_evals)) {
+    trm("evals", n_evals = term_evals)
+  } else if (!is.null(term_time)) {
+    trm("run_time", secs = term_time)
+  }
+}
---
+++ tests/testthat/test_auto_tuner.R
@@ -0,0 +1,22 @@
+test_that("auto_tuner function works", {
+  learner = lrn("classif.rpart")
+  learner$param_set$values$minsplit = to_tune(1, 10)
+
+  at = auto_tuner(method = "random_search", learner = learner, resampling = rsmp ("holdout"), 
+    measure = msr("classif.ce"), term_evals = 50, batch_size = 10)
+
+  expect_class(at, "AutoTuner")
+  expect_class(at$instance_args$terminator, "TerminatorEvals")
+
+  at = auto_tuner(method = "random_search", learner = learner, resampling = rsmp ("holdout"), 
+    measure = msr("classif.ce"), term_time = 50, batch_size = 10)
+
+  expect_class(at, "AutoTuner")
+  expect_class(at$instance_args$terminator, "TerminatorRunTime")
+
+  at = auto_tuner(method = "random_search", learner = learner, resampling = rsmp ("holdout"), 
+    measure = msr("classif.ce"), term_evals = 10, term_time = 50, batch_size = 10)
+
+  expect_class(at, "AutoTuner")
+  expect_class(at$instance_args$terminator, "TerminatorCombo")
+})
