1762e5ac0426618a2e59ac096530ccfec60f3360
Marc Becker <marcbecker@posteo.de>
2021-07-05 18:38:51 +0200

    update examples

--- R/TunerCmaes.R
+++ R/TunerCmaes.R
@@ -16,29 +16,7 @@
 #' @family Tuner
 #' @seealso Package \CRANpkg{mlr3hyperband} for hyperband tuning.
 #' @export
-#' @examples
-#' library(mlr3)
-#' library(paradox)
-#' library(data.table)
-#' search_space = ParamSet$new(list(
-#'   ParamDbl$new("cp", lower = 0.001, upper = 0.1)
-#' ))
-#' terminator = trm("evals", n_evals = 10)
-#' instance = TuningInstanceSingleCrit$new(
-#'   task = tsk("iris"),
-#'   learner = lrn("classif.rpart"),
-#'   resampling = rsmp("holdout"),
-#'   measure = msr("classif.ce"),
-#'   search_space = search_space,
-#'   terminator = terminator
-#' )
-#' tt = tnr("cmaes")
-#' # modifies the instance by reference
-#' tt$optimize(instance)
-#' # returns best configuration and best performance
-#' instance$result
-#' # allows access of data.table of full path of all evaluations
-#' instance$archive
+#' @template example
 TunerCmaes = R6Class("TunerCmaes",
   inherit = TunerFromOptimizer,
   public = list(
