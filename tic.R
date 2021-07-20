9c23feb22b78079620af07aa09330d9cc323b2ed
Marc Becker <marcbecker@posteo.de>
2021-07-20 15:18:13 +0200

    test 1

--- tic.R
+++ tic.R
@@ -8,3 +8,8 @@ if (ci_on_ghactions() && ci_has_env("BUILD_PKGDOWN")) {
     add_step(step_install_github("mlr-org/mlr3pkgdowntemplate"))
   do_pkgdown()
 }
+
+if (ci_on_ghactions() && ci_is_env("BBOTK", "devel")) {
+    get_stage("install") %>%
+      add_step(step_install_github("mlr-org/bbotk"))
+}
