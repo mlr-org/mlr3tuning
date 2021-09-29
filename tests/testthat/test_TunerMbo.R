9c799ae96ce2956d262df1ddb5f631f2f9bdee48
sumny <lennart.sch@web.de>
2021-09-29 15:09:16 +0200

    tests for mbo

--- tests/testthat/test_TunerMbo.R
+++ tests/testthat/test_TunerMbo.R
@@ -1,7 +1,6 @@
-# FIXME: more tests; uncomment when mlr-org/mlr3mbo is stable
-#test_that("TunerMbo", {
-#  library(mlr3learners)
-#  test_tuner("mbo", term_evals = 6L)
-#  test_tuner("mbo", n_dim = 2L, term_evals = 10L)
-#})
+test_that("TunerMbo", {
+  library(mlr3learners)
+  test_tuner("mbo", term_evals = 6L)
+  test_tuner("mbo", n_dim = 2L, term_evals = 10L)
+})
 
