066380aa253767f812c9b11d772dba76cc30efac
Marc Becker <marcbecker@posteo.de>
2021-10-07 17:55:14 +0200

    check triple

--- tests/testthat/test_hotstart.R
+++ tests/testthat/test_hotstart.R
@@ -1,5 +1,7 @@
 test_that("hotstart works", {
   task = tsk("pima")
+
+  # forward
   learner = lrn("classif.debug", x = to_tune(), iter = to_tune(1, 100))
 
   instance = tune(
@@ -13,9 +15,43 @@ test_that("hotstart works", {
     allow_hotstart = TRUE
   )
 
-  ids = map(extract_bmr_learners(instance$archive$benchmark_result), function(l) {
-    l$model$id
-  })
-
+  ids = map(extract_bmr_learners(instance$archive$benchmark_result), function(l) l$model$id)
   expect_equal(length(unique(ids)), 5)
+    expect_equal(unique(instance$archive$data$iter), c(1, 25, 50, 75, 100))
+
+  # backward
+  learner$properties[learner$properties %in% "hotstart_forward"] = "hotstart_backward"
+
+  instance = tune(
+    method = "grid_search",
+    task = task,
+    learner = learner,
+    resampling = rsmp("holdout"),
+    measure = msr("classif.ce"),
+    batch_size = 5,
+    resolution = 5,
+    allow_hotstart = TRUE
+  )
+
+  ids = map(extract_bmr_learners(instance$archive$benchmark_result), function(l) l$model$id)
+  expect_equal(length(unique(ids)), 5)
+  expect_equal(unique(instance$archive$data$iter), c(100, 75, 50, 25, 1))
+
+  # forward and backward
+  learner$properties = c(learner$properties, "hotstart_backward")
+
+  instance = tune(
+    method = "grid_search",
+    task = task,
+    learner = learner,
+    resampling = rsmp("holdout"),
+    measure = msr("classif.ce"),
+    batch_size = 5,
+    resolution = 5,
+    allow_hotstart = TRUE
+  )
+
+  ids = map(extract_bmr_learners(instance$archive$benchmark_result), function(l) l$model$id)
+  expect_equal(length(unique(ids)), 5)
+  expect_equal(unique(instance$archive$data$iter), c(100, 75, 50, 25, 1))
 })
