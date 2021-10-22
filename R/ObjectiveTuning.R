484e917294ca91197729b40e40b995375c0c1dfa
be-marc <marcbecker@posteo.de>
2021-10-22 13:18:11 +0200

    typo

--- R/ObjectiveTuning.R
+++ R/ObjectiveTuning.R
@@ -59,7 +59,7 @@ ObjectiveTuning = R6Class("ObjectiveTuning",
       self$allow_hotstart = assert_logical(allow_hotstart)
       if (self$allow_hotstart) {
         store_models = TRUE
-        self$hoststart_stack = HotstartStack$new()
+        self$hotststart_stack = HotstartStack$new()
       }
       self$store_models = assert_logical(store_models)
 
