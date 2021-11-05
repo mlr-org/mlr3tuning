41ef1a5c5feea2d060f0fa2c0387460b941a7ed0
be-marc <marcbecker@posteo.de>
2021-11-05 18:26:55 +0100

    fix index

--- R/HotstartStackDB.R
+++ R/HotstartStackDB.R
@@ -75,12 +75,12 @@ HotstartStackDB = R6Class("HotstartStack",
         self$add(learners)
 
         # set index for fast subsetting
-
-        DBI::dbDisconnect(con, shutdown = TRUE)
+        DBI::dbExecute(con, "CREATE INDEX task_learner_index ON stack (task_hash, learner_hash)")
         private$.learner_count = length(learners)
       }  else {
         private$.learner_count = 0
       }
+      DBI::dbDisconnect(con, shutdown = TRUE)
     },
 
     #' @description
