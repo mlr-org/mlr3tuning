a377ed1ab08325ef52a724e0a51bc68568514620
be-marc <marcbecker@posteo.de>
2022-02-04 19:38:00 +0100

    docs: evaluate_default

--- man-roxygen/param_evaluate_default.R
+++ man-roxygen/param_evaluate_default.R
@@ -1,3 +1,3 @@
 #' @param evaluate_default (`logical(1)`)\cr
-#'   If `TRUE`, default values of hyperparameters are evaluated at the start
-#'   of the optimization.
+#'   If `TRUE`, learner is evaluated with hyperparameters set to their default
+#'   values at the start of the optimization.
