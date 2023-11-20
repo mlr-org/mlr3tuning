9518cc0ccac8db3cbc221e9f15c791ac33660b3a
be-marc <marcbecker@posteo.de>
2023-11-20 17:48:31 +0100

    refactor: remove benchmark

--- benchmark/batchtools.conf.R
+++
@@ -1,3 +0,0 @@
-cluster.functions = batchtools::makeClusterFunctionsSlurm("/home/mbecke16/slurm_wyoming.tmpl", array.jobs = FALSE)
-default.resources = list(walltime = 28800L, memory = 4000L, ntasks = 1L, ncpus = 32L, nodes = 1L, clusters = "teton")
-max.concurrent.jobs = 4000
