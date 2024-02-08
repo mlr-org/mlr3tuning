e0dc48a986e372d923e26ecb07b6f1113ab6a6e3
be-marc <marcbecker@posteo.de>
2024-02-08 13:53:52 +0100

    tests: pipeline

--- tests/testthat/test_decentralized_optimization.R
+++ tests/testthat/test_decentralized_optimization.R
@@ -71,3 +71,34 @@ test_that("random search works with dependencies", {
   expect_rush_reset(instance$rush)
 })
 
+test_that("random search works with dependencies", {
+  library(rush)
+  library(mlr3pipelines)
+
+  rush_plan(n_workers = 2)
+
+  graph_learner = as_learner(ppl("branch", graphs = list(rpart = lrn("classif.rpart", id = "rpart"),debug = lrn("classif.debug", id = "debug"))))
+  graph_learner$param_set$set_values(
+    "rpart.cp" = to_tune(p_dbl(1e-04, 1e-1, depends = branch.selection == "rpart")),
+    "rpart.minsplit" = to_tune(p_int(2, 128, depends = branch.selection == "rpart")),
+    "debug.x" = to_tune(p_dbl(0, 1, depends = branch.selection == "debug")),
+    "branch.selection" = to_tune(c("rpart", "debug"))
+  )
+
+  instance = TuningInstanceRushSingleCrit$new(
+    task = tsk("pima"),
+    learner = graph_learner,
+    resampling = rsmp("cv", folds = 3),
+    measure = msr("classif.ce"),
+    terminator = trm("evals", n_evals = 20),
+    store_benchmark_result = FALSE
+  )
+
+  optimizer = tnr("random_search_v2")
+  optimizer$optimize(instance)
+
+  expect_data_table(instance$archive$data, min.rows = 20)
+
+  expect_rush_reset(instance$rush)
+})
+
