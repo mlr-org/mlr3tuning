53e7a7079a8d9f9f5f1392c1778d1d038588ee44
Sebastian Fischer <sebf.fischer@gmail.com>
2024-02-26 14:09:57 +0100

    cleanup

--- inst/testthat/helper_misc.R
+++ inst/testthat/helper_misc.R
@@ -140,7 +140,5 @@ MAKE_GL = function() {
   g$add_pipeop(op_lrn)
   g$add_edge("subsample", "classif.rpart")
   gl = GraphLearner$new(g)
-  # FIXME: this is needed because graphlearner sets its properties to all available properties,
-  # which will be fixed in the next pipelines release
   gl
 }
