b6863fc7937620c1fa1a37fe7b2cbc5e19b65689
be-marc <marcbecker@posteo.de>
2024-08-01 11:30:13 +0200

    fix: irace

--- tests/testthat/test_TunerBatchIrace.R
+++ tests/testthat/test_TunerBatchIrace.R
@@ -1,7 +1,7 @@
 skip_if_not_installed("irace")
 
 test_that("TunerIrace", {
-  z = test_tuner("irace", term_evals = 42, real_evals = 39)
+  z = test_tuner("irace", term_evals = 42)
   instance = z$inst
   archive = instance$archive$data
   tuner = z$tuner
@@ -11,7 +11,7 @@ test_that("TunerIrace", {
 
   # check optimization direction
   # first elite of the first race should have the lowest average performance
-  load(tuner$param_set$values$logFile)
+  iraceResults = irace::read_logfile(tuner$param_set$values$logFile)
   elites = iraceResults$allElites
   aggr = archive[race == 1, .(classif.ce = mean(classif.ce)), by = configuration]
   expect_equal(aggr[which.min(classif.ce), configuration], elites[[1]][1])
@@ -26,8 +26,13 @@ test_that("TunerIrace works with dependencies", {
     cp = p_dbl(lower = 0.001, upper = 0.1),
     minsplit = p_int(lower = 1, upper = 10, depends = cp == 0.005)
   )
-  instance = TuningInstanceBatchSingleCrit$new(tsk("iris"), lrn("classif.rpart"), rsmp("holdout"), msr("classif.ce"),
-    trm("evals", n_evals = 96), search_space)
+  instance = ti(
+    task = tsk("mtcars"),
+    learner = lrn("regr.rpart"),
+    resampling = rsmp("holdout"),
+    measures = msr("regr.mse"),
+    terminator = trm("evals", n_evals = 96),
+    search_space = search_space)
   tuner = tnr("irace")
   tuner$optimize(instance)
 
@@ -37,9 +42,16 @@ test_that("TunerIrace works with dependencies", {
 })
 
 test_that("TunerIrace works with logical parameters", {
-  search_space = ps(keep_model = p_lgl())
-  instance = TuningInstanceBatchSingleCrit$new(tsk("mtcars"), lrn("regr.rpart"), rsmp("holdout"), msr("regr.mse"),
-    trm("evals", n_evals = 42), search_space)
+  search_space = ps(
+     cp = p_dbl(lower = 0.001, upper = 0.1),
+    keep_model = p_lgl())
+  instance = ti(
+    task = tsk("mtcars"),
+    learner = lrn("regr.rpart"),
+    resampling = rsmp("holdout"),
+    measures = msr("regr.mse"),
+    terminator = trm("evals", n_evals = 96),
+    search_space = search_space)
   tuner = tnr("irace")
   tuner$optimize(instance)
   expect_logical(instance$archive$best()$keep_model)
@@ -47,8 +59,13 @@ test_that("TunerIrace works with logical parameters", {
 
 test_that("TunerIrace uses digits", {
   search_space = ps(cp = p_dbl(lower = pi * 1e-20, upper = 5.242e12 / 1e13))
-  instance = TuningInstanceBatchSingleCrit$new(tsk("iris"), lrn("classif.rpart"), rsmp("holdout"), msr("classif.ce"),
-  trm("evals", n_evals = 30), search_space)
+ instance = ti(
+    task = tsk("mtcars"),
+    learner = lrn("regr.rpart"),
+    resampling = rsmp("holdout"),
+    measures = msr("regr.mse"),
+    terminator = trm("evals", n_evals = 96),
+    search_space = search_space)
   tuner = tnr("irace", nbIterations = 1L, minNbSurvival = 1)
   expect_data_table(tuner$optimize(instance))
 })
@@ -56,8 +73,13 @@ test_that("TunerIrace uses digits", {
 test_that("TunerIrace works with unnamed discrete values", {
   # we had a bug here, see (mlr) issue #627
   search_space = ps(minsplit = p_int(lower = 2L, upper = 7L))
-  inst = TuningInstanceBatchSingleCrit$new(tsk("iris"), lrn("classif.rpart"), rsmp("holdout"), msr("classif.ce"),
-    trm("evals", n_evals = 50), search_space)
+  instance = ti(
+    task = tsk("mtcars"),
+    learner = lrn("regr.rpart"),
+    resampling = rsmp("holdout"),
+    measures = msr("regr.mse"),
+    terminator = trm("evals", n_evals = 96),
+    search_space = search_space)
   tuner = tnr("irace")
-  expect_data_table(tuner$optimize(inst))
+  expect_data_table(tuner$optimize(instance))
 })
