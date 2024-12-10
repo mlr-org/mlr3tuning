85c19463219b03dc5f56ede58765d48a1541b0ae
Marc Becker <33069354+be-marc@users.noreply.github.com>
2024-12-10 10:40:44 +0100

    tests: skip irace tests (#481)

--- R/TunerBatchIrace.R
+++ R/TunerBatchIrace.R
@@ -53,7 +53,9 @@
 #'
 #' # load learner and set search space
 #' learner = lrn("classif.rpart", cp = to_tune(1e-04, 1e-1, logscale = TRUE))
-#' \donttest{
+#'
+#' # currently there is bug in irace 4.0.0 that sometimes let the example crash
+#' \dontrun{
 #' # hyperparameter tuning on the pima indians diabetes data set
 #' instance = tune(
 #'   tuner = tnr("irace"),
