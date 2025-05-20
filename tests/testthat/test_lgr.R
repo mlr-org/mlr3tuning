c683d04119773df5c3a89ba0c8d17486e3723f42
be-marc <marcbecker@posteo.de>
2025-05-20 17:23:14 +0200

    feat: add mlr3 base logger

--- tests/testthat/test_lgr.R
+++ tests/testthat/test_lgr.R
@@ -1,13 +1,16 @@
 test_that("logger works", {
   on.exit({
-    lgr::get_logger("mlr3")$set_threshold(NULL)
-    lgr::get_logger("mlr3/core")$set_threshold(NULL)
+    lgr::get_logger("mlr3")$set_threshold(0)
+    lgr::get_logger("mlr3/core")$set_threshold(0)
+    lgr::get_logger("mlr3/bbotk")$set_threshold(0)
   })
 
-  lgr::logger_tree()
+  # reset loggers
+  lgr::get_logger("mlr3")$set_threshold(NULL)
+  lgr::get_logger("mlr3/core")$set_threshold(NULL)
+  lgr::get_logger("mlr3/bbotk")$set_threshold(NULL)
 
-  devtools::load_all("../mlr3")
-
+  # log level is inherited from root logger
   res = capture_output(tune(
     tuner = tnr("random_search"),
     task = tsk("pima"),
@@ -19,7 +22,7 @@ test_that("logger works", {
   expect_match(res, "\\[mlr3\\]")
   expect_match(res, "\\[bbotk\\]")
 
-
+  # turn off mlr3 core and bbotk logging via parent logger
   lgr::get_logger("mlr3")$set_threshold("error")
   res = capture_output(tune(
     tuner = tnr("random_search"),
@@ -31,6 +34,7 @@ test_that("logger works", {
   ))
   expect_equal(res, "")
 
+  # turn on mlr3 core and bbotk logging via parent logger
   lgr::get_logger("mlr3")$set_threshold("info")
   res = capture_output(tune(
     tuner = tnr("random_search"),
@@ -43,6 +47,7 @@ test_that("logger works", {
   expect_match(res, "\\[mlr3\\]")
   expect_match(res, "\\[bbotk\\]")
 
+  # turn off bbotk logging
   lgr::get_logger("mlr3/bbotk")$set_threshold("error")
   res = capture_output(tune(
     tuner = tnr("random_search"),
@@ -55,6 +60,7 @@ test_that("logger works", {
   expect_match(res, "\\[mlr3\\]")
   expect_no_match(res, "\\[bbotk\\]")
 
+  # turn on bbotk logging and turn off mlr3 core logging
   lgr::get_logger("mlr3/bbotk")$set_threshold("info")
   lgr::get_logger("mlr3/core")$set_threshold("error")
   res = capture_output(tune(
@@ -65,60 +71,126 @@ test_that("logger works", {
     measures = msr("classif.acc"),
     term_evals = 2L
   ))
+
   expect_no_match(res, "\\[mlr3\\]")
   expect_match(res, "\\[bbotk\\]")
 
-
-
-  lgr::get_logger("mlr3")$set_threshold("info")
-  res = capture_output(resample(tsk("pima"), lrn("classif.featureless"), rsmp("cv", folds = 3L)))
-  expect_match(res, "\\[mlr3\\]")
-
-  lgr::get_logger("mlr3/core")$set_threshold("error")
-  res = capture_output(resample(tsk("pima"), lrn("classif.featureless"), rsmp("cv", folds = 3L)))
-  expect_equal(res, "")
-
   # if the child logger is configured, it overrides the parent logger
   lgr::get_logger("mlr3")$set_threshold("info")
-  res = capture_output(resample(tsk("pima"), lrn("classif.featureless"), rsmp("cv", folds = 3L)))
+  lgr::get_logger("mlr3/core")$set_threshold("error")
+  lgr::get_logger("mlr3/bbotk")$set_threshold("error")
+  res = capture_output(tune(
+    tuner = tnr("random_search"),
+    task = tsk("pima"),
+    learner = lrn("classif.featureless"),
+    resampling = rsmp("cv", folds = 3L),
+    measures = msr("classif.acc"),
+    term_evals = 2L
+  ))
   expect_equal(res, "")
 })
 
 test_that("thresholds are restored on workers", {
 
-  on.exit({
-    lgr::get_logger("mlr3")$set_threshold(NULL)
-    lgr::get_logger("mlr3/core")$set_threshold(NULL)
+   on.exit({
+    lgr::get_logger("mlr3")$set_threshold(0)
+    lgr::get_logger("mlr3/core")$set_threshold(0)
+    lgr::get_logger("mlr3/bbotk")$set_threshold(0)
   })
 
+  # reset loggers
+  lgr::get_logger("mlr3")$set_threshold(NULL)
+  lgr::get_logger("mlr3/core")$set_threshold(NULL)
+  lgr::get_logger("mlr3/bbotk")$set_threshold(NULL)
+
+  # log level is inherited from root logger
   res = capture_output(with_future(future::multisession, {
-    resample(tsk("pima"), lrn("classif.featureless"), rsmp("cv", folds = 3L))
+    tune(
+      tuner = tnr("random_search"),
+      task = tsk("pima"),
+      learner = lrn("classif.featureless"),
+      resampling = rsmp("cv", folds = 3L),
+      measures = msr("classif.acc"),
+      term_evals = 2L
+    )
   }))
   expect_match(res, "\\[mlr3\\]")
+  expect_match(res, "\\[bbotk\\]")
 
+  # turn off mlr3 core and bbotk logging via parent logger
   lgr::get_logger("mlr3")$set_threshold("error")
-
   res = capture_output(with_future(future::multisession, {
-    resample(tsk("pima"), lrn("classif.featureless"), rsmp("cv", folds = 3L))
+    tune(
+      tuner = tnr("random_search"),
+      task = tsk("pima"),
+      learner = lrn("classif.featureless"),
+      resampling = rsmp("cv", folds = 3L),
+      measures = msr("classif.acc"),
+      term_evals = 2L
+    )
   }))
   expect_equal(res, "")
 
+  # turn on mlr3 core and bbotk logging via parent logger
   lgr::get_logger("mlr3")$set_threshold("info")
   res = capture_output(with_future(future::multisession, {
-    resample(tsk("pima"), lrn("classif.featureless"), rsmp("cv", folds = 3L))
+    tune(
+      tuner = tnr("random_search"),
+      task = tsk("pima"),
+      learner = lrn("classif.featureless"),
+      resampling = rsmp("cv", folds = 3L),
+      measures = msr("classif.acc"),
+      term_evals = 2L
+    )
   }))
   expect_match(res, "\\[mlr3\\]")
+  expect_match(res, "\\[bbotk\\]")
 
+  # turn off bbotk logging
+  lgr::get_logger("mlr3/bbotk")$set_threshold("error")
+  res = capture_output(with_future(future::multisession, {
+    tune(
+      tuner = tnr("random_search"),
+      task = tsk("pima"),
+      learner = lrn("classif.featureless"),
+      resampling = rsmp("cv", folds = 3L),
+      measures = msr("classif.acc"),
+      term_evals = 2L
+    )
+  }))
+  expect_match(res, "\\[mlr3\\]")
+  expect_no_match(res, "\\[bbotk\\]")
+
+  # turn on bbotk logging and turn off mlr3 core logging
+  lgr::get_logger("mlr3/bbotk")$set_threshold("info")
   lgr::get_logger("mlr3/core")$set_threshold("error")
   res = capture_output(with_future(future::multisession, {
-    resample(tsk("pima"), lrn("classif.featureless"), rsmp("cv", folds = 3L))
+    tune(
+      tuner = tnr("random_search"),
+      task = tsk("pima"),
+      learner = lrn("classif.featureless"),
+      resampling = rsmp("cv", folds = 3L),
+      measures = msr("classif.acc"),
+      term_evals = 2L
+    )
   }))
-  expect_equal(res, "")
+
+  expect_no_match(res, "\\[mlr3\\]")
+  expect_match(res, "\\[bbotk\\]")
 
   # if the child logger is configured, it overrides the parent logger
   lgr::get_logger("mlr3")$set_threshold("info")
+  lgr::get_logger("mlr3/core")$set_threshold("error")
+  lgr::get_logger("mlr3/bbotk")$set_threshold("error")
   res = capture_output(with_future(future::multisession, {
-    resample(tsk("pima"), lrn("classif.featureless"), rsmp("cv", folds = 3L))
+    tune(
+      tuner = tnr("random_search"),
+      task = tsk("pima"),
+      learner = lrn("classif.featureless"),
+      resampling = rsmp("cv", folds = 3L),
+      measures = msr("classif.acc"),
+      term_evals = 2L
+    )
   }))
   expect_equal(res, "")
 })
