a8be5f9c4e8ad45dd82e6d877f54c92fd724e085
be-marc <marcbecker@posteo.de>
2026-01-23 07:55:56 +0100

    docs: tune token and deps

--- man-roxygen/param_search_space.R
+++ man-roxygen/param_search_space.R
@@ -1,4 +1,4 @@
 #' @param search_space ([paradox::ParamSet])\cr
-#'   Hyperparameter search space. If `NULL` (default), the search space is
-#'   constructed from the [paradox::TuneToken] of the learner's parameter set
-#'   (learner$param_set).
+#'   Hyperparameter search space.
+#'   If `NULL` (default), the search space is constructed from the [paradox::TuneToken] of the learner's parameter set (learner$param_set).
+#'   When using `to_tune()` tokens, dependencies for hierarchical search spaces are automatically handled.
